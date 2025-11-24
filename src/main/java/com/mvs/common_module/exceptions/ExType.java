45edeb65725bff1335ddf783aa2b90528cfd9a96
Sahan Vithanage <sahanmvs96@gmail.com>
2025-11-24 11:23:48 +0530

    add cart ex types

--- src/main/java/com/mvs/common_module/exceptions/ExType.java
+++ src/main/java/com/mvs/common_module/exceptions/ExType.java
@@ -3,8 +3,8 @@ package com.mvs.common_module.exceptions;
 public enum ExType implements ExceptionType {
     UNAUTHORIZED, INVALID_CREDENTIALS, INVALID_STATUS, USER_ALREADY_EXISTS, USER_NOT_FOUND,
     PRODUCT_NOT_FOUND, INVENTORY_NOT_FOUND, INSUFFICIENT_STOCK,
-    ORDER_NOT_FOUND;
-
+    ORDER_NOT_FOUND,
+    CART_NOT_FOUND, EMPTY_CART, PRICE_MISMATCH;
     @Override
     public String getExceptionType() {
         return this.toString();
